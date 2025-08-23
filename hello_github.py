bbc6da2278a6588c0e43f9e674f6210398258ead
AyeshaAtharAli <151917184+AyeshaAtharAli@users.noreply.github.com>
2025-08-23 16:15:28 +0500

    Delete hello_github.py

--- hello_github.py
+++
@@ -1,4 +0,0 @@
-print("Hello, GitHub!")
-print("This is Ayesha Athar.A Computer Science Graduate.")
-
-print("My Favorite topics in Machine Learning are Natural Language Processing and Computer Vision and in Artificial Intelligence they are RAG and Agentic AI.")
